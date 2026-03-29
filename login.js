db4af5806624f1c0c453681b22f33b1849d44d15
Nithin A <nithinandcherry@gmail.com>
2026-03-29 22:28:48 +0530

    login backend day 03

---
+++ login.js
@@ -0,0 +1,22 @@
+async function login(){
+
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+const response = await fetch("http://localhost:3000/login",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({email,password})
+});
+
+const data = await response.json();
+
+if(data.success){
+    window.location.href = "dashboard.html";
+}else{
+    alert("Invalid credentials");
+}
+
+}
