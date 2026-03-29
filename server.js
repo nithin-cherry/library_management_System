16a22f5df7a9876c41d7f479f424ce700e000c01
Nithin A <nithinandcherry@gmail.com>
2026-03-29 22:32:58 +0530

    server backend

--- server.js
+++ server.js
@@ -1,37 +1,29 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-app.get("/", (req, res) =>
-{
-    res.send("Server is working ");
-})
-
-
-
+app.use(cors());
 app.use(express.json());
 
-
 app.post("/login", (req, res) => {
-    const { username, password } = req.body;
 
-    const user = user.find(
-        u => u.username === username && u.password === password
-    );
-    if (user)
-        res.sendStatus("Login Successful");
-    else(user)
-    res.send("Invalid Credentials");
+    const { email, password } = req.body;
+
+    console.log("Login attempt:", email, password);
+
+    if(email === "admin@gmail.com" && password === "1234"){
+        res.json({success:true, message:"Login successful"});
+    }
+    else if(email === "nithin@gmail.com" && password === "cherry"){
+        res.json({success:true, message:"Login successful"});
+    }
+    else{
+        res.json({success:false, message:"Invalid credentials"});
+    }
+
 });
 
-
-const users = [
-  { username: "admin", password: "1234" },
-  { username: "nithin", password: "pass123" }
-];
-
-
-app.listen(3000, () => 
-{
-    console.log("Server is runnig at 3000 port");
-})
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
