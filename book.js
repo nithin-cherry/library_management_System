d434790fd2254c5f5140da38b642a0bd69f0faad
Nithin A <nithinandcherry@gmail.com>
2026-03-30 12:59:27 +0530

    books database

---
+++ book.js
@@ -0,0 +1,25 @@
+const books = [
+    {
+        id: 1,
+        title: "Rich Dad Poor Dad",
+        author: "Robert T. Kiyosaki",
+        totalCopies: 10,
+        availableCopies: 7
+    },
+    {
+        id: 2,
+        title: "Bhagavad Gita",
+        author: "Ved Vyasa",
+        totalCopies: 5,
+        availableCopies: 0
+    },
+    {
+        id: 3,
+        title: "48 Laws of Power",
+        author: "Robert Greene",
+        totalCopies: 6,
+        availableCopies: 5
+    }
+];
+
+module.exports = books;
