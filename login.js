10abac98275853fd7bf1465d3a2f351cd169d017
Nithin A <nithinandcherry@gmail.com>
2026-03-30 13:03:33 +0530

    login js

--- login.js
+++ login.js
@@ -4,12 +4,16 @@ const email = document.getElementById("email").value;
 const password = document.getElementById("password").value;
 
 const response = await fetch("http://localhost:3000/login",{
-method:"POST",
+    method: "POST",
+    
 headers:{
 "Content-Type":"application/json"
 },
+
 body:JSON.stringify({email,password})
 });
+    
+    
 
 const data = await response.json();
 
