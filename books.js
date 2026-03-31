b5a96b6e07df0f81e7379eb0145d9d3193127b0f
Nithin A <nithinandcherry@gmail.com>
2026-03-31 19:44:04 +0530

    books day 05

---
+++ books.js
@@ -0,0 +1,28 @@
+const books = [
+    {
+        id: 1,
+        title: "Rich Dad Poor Dad",
+        author: "Robert T. Kiyosaki",
+        url:"https://imgs.search.brave.com/LVAATEFI-icPYpFRHnoRpSikkVuu7p0i62hiuWNPTTo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMtbmEuc3NsLWlt/YWdlcy1hbWF6b24u/Y29tL2ltYWdlcy9J/LzYxc09HUEp5Mm9M/LmpwZw",
+        totalCopies: 10,
+        availableCopies: 7
+    },
+    {
+        id: 2,
+        title: "Bhagavad Gita",
+        author: "Ved Vyasa",
+        url:"https://imgs.search.brave.com/79uKzDaEeJVd_V78Z7aCMCFYIl8C39tz0NOwyf7p70g/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvaGQvYmhh/Z2F2YWQtZ2l0YS1h/cy1pdC1pcy1ib29r/LWNvdmVyLWt3N2ps/aGY2cmEyejk1bmku/anBn",
+        totalCopies: 5,
+        availableCopies: 0
+    },
+    {
+        id: 3,
+        title: "48 Laws of Power",
+        url:"https://imgs.search.brave.com/uQZmTEvlaYgGdnxZ5cZhoPeKQcaeGSbktMx8mZTqEIM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NDFQZkhlZkZxMUwu/anBn",
+        author: "Robert Greene",
+        totalCopies: 6,
+        availableCopies: 5
+    }
+];
+
+module.exports = books;
